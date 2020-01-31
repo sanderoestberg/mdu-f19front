f9583df11a185f8c8064611716ba02c0f470fa01
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-01-31 10:43:30 +0100

    Solution: array_family_members

---
+++ array_family_members/js/main.js
@@ -0,0 +1,123 @@
+/*
+Fetches json data from the file persons.json
+*/
+let familyMembers = [{
+  name: "Peter Madsen",
+  age: 52,
+  hairColor: "blonde",
+  relation: "dad",
+  img: "img/dad.jpg"
+}, {
+  name: "Ane Madsen",
+  age: 51,
+  hairColor: "brown",
+  relation: "mom",
+  img: "img/ane.jpg"
+}, {
+  name: "Rasmus Madsen",
+  age: 29,
+  hairColor: "blonde",
+  relation: "brother",
+  img: "img/rasmus.jpg"
+}, {
+  name: "Mie Madsen",
+  age: 25,
+  hairColor: "brown",
+  relation: "sister",
+  img: "img/mie.jpg"
+}, {
+  name: "Mads Madsen",
+  age: 18,
+  hairColor: "dark",
+  relation: "brother",
+  img: "img/mads.jpg"
+}, {
+  name: "Jens Madsen",
+  age: 14,
+  hairColor: "blonde",
+  relation: "brother",
+  img: "img/jenspeter.jpg"
+}];
+
+console.log(familyMembers);
+
+/*
+Appends json data to the DOM
+*/
+function appendPersons(persons) {
+  let htmlTemplate = "";
+  for (let person of persons) {
+    htmlTemplate += `
+      <article>
+        <img src="${person.img}">
+        <h4>${person.name}</h4>
+        <p>${person.age} years old</p>
+        <p>Hair color: ${person.hairColor}</p>
+        <p>Relation: ${person.relation}</p>
+      </article>
+    `;
+  }
+  document.querySelector("#persons").innerHTML = htmlTemplate;
+}
+
+appendPersons(familyMembers);
+
+function search(searchValue) {
+  searchValue = searchValue.toLowerCase();
+  console.log(searchValue);
+
+  let filteredfamilyMembers = [];
+
+  for (let familyMember of familyMembers) {
+    let name = familyMember.name.toLowerCase();
+    if (name.includes(searchValue.toLowerCase())) {
+      filteredfamilyMembers.push(familyMember);
+    }
+  }
+
+  console.log(filteredfamilyMembers);
+  appendPersons(filteredfamilyMembers);
+}
+
+// function search(searchValue) {
+//   searchValue = searchValue.toLowerCase();
+//   let filteredfamilyMembers = familyMembers.filter(function(familyMember) {
+//     let name = familyMember.name.toLowerCase();
+//     return name.includes(searchValue);
+//   });
+//
+//   console.log(filteredfamilyMembers);
+//   appendPersons(filteredfamilyMembers);
+// }
+
+// function search(searchValue) {
+//   searchValue = searchValue.toLowerCase();
+//   let filteredfamilyMembers = familyMembers.filter(familyMember => familyMember.name.toLowerCase().includes(searchValue));
+//
+//   console.log(filteredfamilyMembers);
+//   appendPersons(filteredfamilyMembers);
+// }
+
+function add() {
+  console.log("Add button clicked");
+
+  let inputName = document.getElementById('inputName');
+  let inputAge = document.getElementById('inputAge');
+  let inputHairColor = document.getElementById('inputHairColor');
+  let inputRelation = document.getElementById('inputRelation');
+  let inputImageUrl = document.getElementById('inputImageUrl');
+
+  console.log(inputName.value, inputAge.value, inputHairColor.value, inputRelation.value, inputImageUrl.value);
+
+  let newPerson = {
+    name: inputName.value,
+    age: inputAge.value,
+    hairColor: inputHairColor.value,
+    relation: inputRelation.value,
+    img: inputImageUrl.value
+  };
+  console.log(newPerson);
+
+  familyMembers.push(newPerson);
+  appendPersons(familyMembers);
+}
