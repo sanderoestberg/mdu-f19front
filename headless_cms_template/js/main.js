700524551de30c3a045e63cad35101d5c1f74a67
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-02-02 15:48:10 +0100

    New projects added.

---
+++ headless_cms_template/js/main.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const url = "http://api.cederdorff.com/wp-json/wp/v2/posts";
+// const url = "http://api.cederdorff.com/wp-json/wp/v2/posts?_embed";
+
+fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(posts) {
+    console.log(posts);
+  });
+
+function appendPosts(posts) {
+  // to do
+}
+
+// get the featured image url
+// function getFeaturedImageUrl(post) {
+//   let imageUrl = "";
+//   if (post._embedded['wp:featuredmedia']) {
+//     imageUrl = post._embedded['wp:featuredmedia'][0].source_url;
+//   }
+//   return imageUrl;
+// }
